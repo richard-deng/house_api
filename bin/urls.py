03d400f84a8d3d21608b78a24cfac042f953327f
邓成 <dengcheng@qfpay.com>
2018-06-20 10:28:23 +0800

    添加

--- bin/urls.py
+++ bin/urls.py
@@ -3,12 +3,14 @@ from handler import (
     page,
     ping,
     box,
+    questions,
 )
 
 urls = (
     ('^/ping$', ping.Ping),
     ('^/v1/api/box/list', box.BoxListHandler),
     ('^/v1/api/box/info', box.BoxInfoHandler),
+    ('^/v1/api/question/children/info', questions.QuestionSingleHandler),
 
 
     # 页面
